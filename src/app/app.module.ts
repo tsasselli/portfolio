cbed04a90739ae165e6caf0fba22cc1a35d9e44a
tsasselli <tsasselli@gmail.com>
2018-07-14 21:57:20 -0700

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
+import { MaterialModule } from './material';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routedComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    routedComponents,
+    NavbarComponent,
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MaterialModule,
+    LayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatListModule, MatSidenavModule } from '@angular/material';
+
+
+@NgModule({
+    imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatListModule, MatSidenavModule],
+    exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatListModule, MatSidenavModule],
+})
+
+export class MaterialModule { };
